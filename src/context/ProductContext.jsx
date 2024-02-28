aeb0e304586e61f096a70e916658c442d436e324
USER <sernajesus032@gmail.com>
2024-02-28 17:23:43 -0500

    Related products when user is adding to cart

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,30 +1,28 @@
-import React,{createContext, useState, useEffect} from 'react'
-
+import React, { createContext, useState, useEffect } from "react";
 
 // context
-export const ProductContext = createContext()
+export const ProductContext = createContext();
 
-
-
-
-const ProductProvider = ({children}) => {
+const ProductProvider = ({ children }) => {
   //products state
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
   // fetch products
-  useEffect(()=>{
-    const fetchProducts = async ()=>{
-      const response = await fetch('https://fakestoreapi.com/products?limit=10');
-      const data = await response.json()
-      setProducts(data)
-      console.log(data)
-      
-
-    }
-    fetchProducts()
-  },[])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://fakestoreapi.com/products?limit=10"
+      );
+      const data = await response.json();
+      setProducts(data);
+      console.log(data);
+    };
+    fetchProducts();
+  }, []);
   return (
-    <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>
-  )
-}
+    <ProductContext.Provider value={{ products }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
 
-export default ProductProvider
+export default ProductProvider;
