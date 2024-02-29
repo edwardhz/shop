9fa6fde61e677986b53a9845134808890ca3a7ce
USER <sernajesus032@gmail.com>
2024-02-29 13:09:45 -0500

    Doing the grid column 2

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -49,10 +49,12 @@ const ProductDetails = () => {
     <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center">
-          <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
-            <img className="max-w-[200px] lg:max-w-sm" src={image} alt="" />
-          </div>
-          <div className="flex-1 text-center lg:text-left">
+          <div className="flex-1 text-center lg:text-left lg:mr-8 mb-8 lg:mb-0">
+            <img
+              className="max-w-[200px] lg:max-w-sm mx-auto mb-4"
+              src={image}
+              alt=""
+            />
             <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">
               {title}
             </h1>
@@ -67,30 +69,34 @@ const ProductDetails = () => {
               Add to cart
             </button>
           </div>
-        </div>
-      </div>
-      {/* Mostrar productos relacionados */}
-      <h3 className="mt-8 mb-4 text-xl font-semibold">
-        Productos relacionados:
-      </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-        {relatedProducts.map((relatedProduct) => (
-          <div key={relatedProduct.id} className="text-center">
-            <img
-              className="max-w-[100px] mx-auto mb-2"
-              src={relatedProduct.image}
-              alt={relatedProduct.description}
-            />
-            <h4 className="text-sm font-semibold">{relatedProduct.title}</h4>
-            <p className="text-sm">${relatedProduct.price}</p>
-            <button
-              onClick={() => addToCart(relatedProduct, relatedProduct.id)}
-              className="bg-primary py-2 px-4 text-white mt-2"
-            >
-              Add to cart
-            </button>
+          <div className="flex-1">
+            {/* Mostrar productos relacionados */}
+            <h3 className="text-xl font-semibold mb-4 flex justify-center items-center">
+              Te podría gustar
+            </h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
+              {relatedProducts.map((relatedProduct) => (
+                <div key={relatedProduct.id} className="text-center">
+                  <img
+                    className="max-w-[100px] mx-auto mb-2"
+                    src={relatedProduct.image}
+                    alt={relatedProduct.description}
+                  />
+                  <h4 className="text-sm font-semibold">
+                    {relatedProduct.title}
+                  </h4>
+                  <p className="text-sm">${relatedProduct.price}</p>
+                  <button
+                    onClick={() => addToCart(relatedProduct, relatedProduct.id)}
+                    className="bg-primary py-2 px-4 text-white mt-2"
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
+        </div>
       </div>
     </section>
   );
